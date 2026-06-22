e9f952282f69cb6277119921204afdea478b8045
FosterFcs <fosterconstrutora@gmail.com>
2026-06-22 14:44:52 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// =========================================================
+// CONFIGURAÇÃO DO SUPABASE
+// Projeto: fomentar-obras (Foster Obras)
+// =========================================================
+
+const SUPABASE_URL = "https://mhxpauvubopbwnhhsoff.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1oeHBhdXZ1Ym9wYnduaGhzb2ZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4OTc5NTQsImV4cCI6MjA5NzQ3Mzk1NH0.tFfrfiAde-qqK-ri9JVnJuF3lH8naZQYtggVB2gR8NI";
